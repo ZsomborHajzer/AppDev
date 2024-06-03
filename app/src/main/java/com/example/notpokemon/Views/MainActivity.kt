bc8c3a4dfbb7f453f0c5fe182e165d72b8f7d34e
ZsomborHajzer <zsombor.hajzer@student.nhlstenden.com>
2024-06-04 00:14:11 +0200

    minor changes and changed theme

--- app/src/main/java/com/example/notpokemon/Views/MainActivity.kt
+++ app/src/main/java/com/example/notpokemon/Views/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.example.notpokemon.Views
-
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.example.notpokemon.ConnectPageActivity
+import com.example.notpokemon.EventHandlers
+import com.example.notpokemon.UIInitializer
+import com.example.notpokemon.WebSocketHandler
+
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var uiInitializer: UIInitializer
+    private lateinit var webSocketHandler: WebSocketHandler
+    private lateinit var eventHandlers: EventHandlers
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MainActivity.instance = this
 
-        // example redirect ;)
-        val intent = Intent(this@MainActivity, BoardView::class.java)
-        startActivity(intent);
+        // Redirect to ConnectPageActivity
+        val intent = Intent(this@MainActivity, ConnectPageActivity::class.java)
+        startActivity(intent)
+        uiInitializer = UIInitializer(this)
+        uiInitializer.initialize()
+
+        webSocketHandler = WebSocketHandler {
+            uiInitializer.receivedMessages.append("\n$it")
+        }
+
+        eventHandlers = EventHandlers(uiInitializer, webSocketHandler)
+        eventHandlers.setupEventHandlers()
     }
 
-    companion object{
+    companion object {
         lateinit var instance: MainActivity
     }
+}
 
 
     //TODO Figure out how to implement flow between views:
@@ -38,4 +56,4 @@ class MainActivity : ComponentActivity() {
 //        eventHandlers = EventHandlers(uiInitializer, webSocketHandler)
 //        eventHandlers.setupEventHandlers()
 //    }
-}
+//}
